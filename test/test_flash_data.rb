3c8aa30708a0d1f94daaae43ba5cf8cdd089e022
milboj <milboj@gmail.com>
2010-03-20 11:26:10 +0100

    first commit

---
+++ test/test_flash_data.rb
@@ -0,0 +1,69 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+require File.join(File.dirname(__FILE__), '../lib/flash_tool.rb')
+
+require 'test/unit'
+
+class Test_Flash_Data < Test::Unit::TestCase
+  include FlashTool
+  CURRENT_DIR = File.dirname(File.expand_path(__FILE__)) + "/"
+  TEST_SWF = CURRENT_DIR + "tester.swf"
+  BAD_FILE = CURRENT_DIR + "bad_ext.txt"
+  JPG_SWF = CURRENT_DIR + "jpeg.swf"
+  NO_FILE = CURRENT_DIR + "jpegswf.swf"
+  BAD_SWF = CURRENT_DIR + "bad_swf.swf"
+  
+  def test_get_info
+    info =  FlashTool.flash_info(TEST_SWF)
+    assert_kind_of Hash, info
+  end
+
+  def test_missing_methods
+    assert_kind_of(Integer, FlashTool.width(TEST_SWF))
+    assert_kind_of(Integer, FlashTool.height(TEST_SWF))
+    assert_kind_of(Integer, FlashTool.frames(TEST_SWF))
+    assert_kind_of(Float, FlashTool.rate(TEST_SWF))
+    assert_kind_of(String, FlashTool.html(TEST_SWF))
+    assert_kind_of(String, FlashTool.xhtml(TEST_SWF))
+    assert_kind_of(String, FlashTool.full(TEST_SWF))
+    assert_kind_of(String, FlashTool.hex(TEST_SWF))
+    assert_kind_of(String, FlashTool.buttons(TEST_SWF))
+    assert_kind_of(String, FlashTool.placements(TEST_SWF))
+    assert_kind_of(String, FlashTool.fonts(TEST_SWF))
+
+  end
+
+  def test_bad_file
+
+    assert_raise(FlashToolError) { FlashTool.flash_info(BAD_FILE) }
+    assert_raise(FlashToolError) { FlashTool.full(BAD_FILE) }
+    assert_raise(FlashToolError) { FlashTool.width(BAD_FILE) }
+    assert_raise(FlashToolError) { FlashTool.parse_text(BAD_FILE) }
+    assert_raise(FlashToolError) { FlashTool.swfdump(BAD_FILE, 'width') }
+  end
+
+  def test_no_file
+
+    assert_raise(FlashToolError) { FlashTool.flash_info(NO_FILE) }
+    assert_raise(FlashToolError) { FlashTool.full(NO_FILE) }
+    assert_raise(FlashToolError) { FlashTool.width(NO_FILE) }
+    assert_raise(FlashToolError) { FlashTool.parse_text(NO_FILE) }
+    assert_raise(FlashToolError) { FlashTool.swfdump(NO_FILE, 'width') }
+  end
+
+  # Testing bad swf
+  def test_bad_swf_file
+
+    assert_raise(FlashToolError) { FlashTool.flash_info(BAD_SWF) }
+    assert_raise(FlashToolError) { FlashTool.full(BAD_SWF) }
+    assert_raise(FlashToolError) { FlashTool.width(BAD_SWF) }
+    assert_raise(FlashToolError) { FlashTool.parse_text(BAD_SWF) }
+    assert_raise(FlashToolError) { FlashTool.swfdump(BAD_SWF, 'width') }
+  end
+
+  def test_parse_text
+    assert_equal "",FlashTool.parse_text(JPG_SWF) #image flash
+    assert_true(FlashTool.parse_text(TEST_SWF).length > 100 ) #test number of charachters
+  end
+end
---
+++ test/test_flash_tool.rb
@@ -0,0 +1,162 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+require 'test/unit'
+require File.join(File.dirname(__FILE__), '../lib/flash_tool.rb')
+
+class FlashToolsTest < Test::Unit::TestCase
+  include FlashTool
+
+  CURRENT_DIR = File.dirname(File.expand_path(__FILE__)) + "/"
+
+  PDF_FILE  = CURRENT_DIR + "test.pdf"
+  PDF_PASSW_PROTECTED = CURRENT_DIR + "test_with_password.pdf"
+  GIF_FILE  = CURRENT_DIR + "25-1.gif"
+  FONT_FILE = CURRENT_DIR + "liberationserif_bold.ttf"
+  JPG_FILE  = CURRENT_DIR + "test.jpg"
+  PNG_FILE  = CURRENT_DIR + "test.png"
+  VIEW_SWF  = CURRENT_DIR + "rfxview.swf"
+
+
+
+  def test_flash_object_from_blob
+    File.open(JPG_FILE, "rb") do |f|
+      flash_object = FlashObject.from_blob(f.read, 'jpg')
+    end
+  end
+
+
+  def test_flash_object_new
+    flash_object = FlashObject.new(JPG_FILE)
+  end
+
+  #
+  # Testing file type detection and creation by file type
+  #
+  #
+
+  def test_pdf_create
+    begin
+      output_file = "#{CURRENT_DIR}/test.swf"
+      flash_object = FlashObject.new(PDF_FILE)
+      flash_object.jpegquality(80)
+      flash_object.save(output_file)
+
+      assert(File.exist?(output_file))
+    ensure
+      File.delete(output_file)
+    end
+  end
+  def test_pdf_create_with_password
+    output_file = "#{CURRENT_DIR}/test_password.swf"
+    begin
+      flash_object = FlashObject.new(PDF_PASSW_PROTECTED)
+      flash_object.jpegquality(80)
+      flash_object.password("test")
+      flash_object.save(output_file)
+      assert(File.exist?(output_file))
+    ensure
+      File.delete(output_file)
+    end
+  end
+
+  def test_pdf_create_with_password_without_save_declaration
+    output_file = "#{CURRENT_DIR}/test_password.swf"
+    begin
+      flash_object = FlashObject.new(PDF_PASSW_PROTECTED)
+      flash_object.jpegquality(80)
+      flash_object.password("test")
+      flash_object.output(output_file)
+      flash_object.save()
+      assert(File.exist?(output_file))
+    ensure
+      File.delete(output_file)
+    end
+  end
+
+  def test_font_create
+    output_file = "#{CURRENT_DIR}/arial.swf"
+    begin
+      flash_object = FlashObject.new(FONT_FILE)
+      flash_object.save(output_file)
+      assert(File.exist?(output_file))
+    ensure
+      File.delete(output_file)
+    end
+  end
+
+  def test_gif_create
+    output_file = "#{CURRENT_DIR}/gif.swf"
+    begin
+      flash_object = FlashObject.new(GIF_FILE)
+      flash_object.save(output_file)
+      assert(File.exist?(output_file))
+    ensure
+      File.delete(output_file)
+    end
+  end
+
+  def test_png_create
+    output_file = "#{CURRENT_DIR}/png.swf"
+    begin
+      flash_object = FlashObject.new(PNG_FILE)
+      flash_object.save(output_file)
+      assert(File.exist?(output_file))
+    ensure
+      File.delete(output_file)
+    end
+  end
+
+  def test_jpg_create
+    output_file = "#{CURRENT_DIR}/jpg.swf"
+    begin
+      flash_object = FlashObject.new(JPG_FILE)
+      flash_object.save(output_file)
+      assert(File.exist?(output_file))
+    ensure
+      File.delete(output_file)
+    end
+  end
+
+   def test_create_from_blob
+    output_file = "#{CURRENT_DIR}/jpg.swf"
+    begin
+      tt = File.open(JPG_FILE, "rb")
+      flash_object = FlashObject.from_blob(tt.read, 'jpg')
+      flash_object.quality(80)
+      flash_object.save(output_file)
+    ensure
+      File.delete(output_file)
+    end
+  end
+
+   def test_creating_viewer
+   output_file = "#{CURRENT_DIR}/jpg.swf"
+      output_file = "#{CURRENT_DIR}/test_viewer.swf"
+    begin
+      flash_object = FlashObject.new(PDF_FILE)
+      flash_object.jpegquality(80)
+      flash_object.viewer(VIEW_SWF)
+      flash_object.save(output_file)
+      assert(File.exist?(output_file))
+    ensure
+      File.delete(output_file)
+    end
+   end
+
+
+  #
+  # Testing errors and bad types
+  #
+
+  def test_file_not_exist
+    assert_raise(FlashToolError) { flash_object = FlashObject.new("nofile.jpg")   }
+  end
+
+  def test_wrong_file_type
+    assert_raise(FlashToolError) {flash_object = FlashObject.new("bad_ext.txt")}
+  end
+
+
+end
+
